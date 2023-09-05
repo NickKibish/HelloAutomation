5bd615c5281f013cbf8697d4c983c3e2ca09a594
Nick Kibysh <nick.kibysh@gmail.com>
2023-09-05 10:38:25 +0200

    added build actions

--- Tests/HelperTests/HelperTests.swift
+++ Tests/HelperTests/HelperTests.swift
@@ -6,5 +6,6 @@ final class HelperTests: XCTestCase {
         let helper = Helper()
         let date = Date(timeIntervalSince1970: 1634130000)
         XCTAssertEqual(helper.now(date), "2021-10-13 15:00:00")
+        
     }
 }
