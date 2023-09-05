59e97ac439e5aa2d515e432a46b8e99ddd4fa2cb
Nick Kibysh <nick.kibysh@gmail.com>
2023-09-05 11:00:30 +0200

    fixed tests

--- Tests/HelperTests/HelperTests.swift
+++ Tests/HelperTests/HelperTests.swift
@@ -4,8 +4,20 @@ import XCTest
 final class HelperTests: XCTestCase {
     func testExample() throws {
         let helper = Helper()
-        let date = Date(timeIntervalSince1970: 1634130000)
+
+        let dateComponents = DateComponents(
+            calendar: Calendar.current,
+            timeZone: TimeZone.current,
+            year: 2021,
+            month: 10,
+            day: 13,
+            hour: 15,
+            minute: 0,
+            second: 0
+        )
+
+        let date = dateComponents.date!
         XCTAssertEqual(helper.now(date), "2021-10-13 15:00:00")
-        
+
     }
 }
